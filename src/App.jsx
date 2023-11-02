a42f07daba6514ef51db5b033808838de350847f
TARishabh <127947846+TARishabh@users.noreply.github.com>
2023-11-02 14:18:30 +0530

    changed autocrlf and safecrlf to false

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
-import News from './components/News'
-import NewsItem from './components/NewsItem'
-import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-import React, { Component } from 'react'
+function App() {
+  const ref = useRef(null);
+  const loadingBarRef = useRef(null);
+  const apiKey = import.meta.env.VITE_REACT_APP_NEWS_API;
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navbar/>
-          <Routes>
-            <Route exact path = '#' element = {<News key="general" category={"general"}/>}/>
-            <Route exact path = '/about' element = {<News key="general"category={"general"}/>}/>
-            <Route exact path = '/home' element = {<News key="general"category={"general"}/>}/>
-            <Route exact path = '/sports' element = {<News key="sports"category={"sports"}/>}/>
-            <Route exact path = '/business' element = {<News key="business" category={"business"}/>}/>
-            <Route exact path = '/science' element = {<News key="science" category={"science"}/>}/>
-          </Routes>
-          
-        </div>
-      </Router>
-    )
-  }
+  const setProgress = (progress) => {
+    if (loadingBarRef.current) {
+      loadingBarRef.current.staticStart();
+      loadingBarRef.current.continuousStart();
+      loadingBarRef.current.complete();
+    }
+  };
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <LoadingBar color='#f11946' ref={ref} height='5px'/>
+        {/* <button onClick={() => ref.current.continuousStart()}>
+          Start Continuous Loading Bar
+        </button>
+        <button onClick={() => ref.current.staticStart()}>
+          Start Static Loading Bar
+        </button>
+        <button onClick={() => ref.current.complete()}>Complete</button> */}
+        <br />
+        <Routes>
+          <Route path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="general" category={"general"} />} />
+          <Route path='/about' element={<News setProgress={setProgress} apiKey={apiKey} key="general" category={"general"} />} />
+          <Route path='/home' element={<News setProgress={setProgress} apiKey={apiKey} key="general" category={"general"} />} />
+          <Route path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" category={"sports"} />} />
+          <Route path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" category={"business"} />} />
+          <Route path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" category={"science"} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-
-// import React, { Component } from 'react'
-// import NewsItem from './NewsItem';
-
-// export default class News extends Component {
-//     constructor(props){
-//         super(props);
-//         console.log("constructor hoon BC")
-//         this.state = {
-//             articles:[],
-//             page:1
-//         }
-//     }
-
-//     gotonextpage = async () =>{
-//         try{
-//             let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=ce925a4554cb4328805b38890d26b6df&page=${this.state.page + 1}`);
-//             let parsed_data = await data.json();
-//             this.setState({
-//                 page:this.state.page + 1,
-//                 articles: parsed_data.articles,
-//             });
-//         }
-//         catch (error){
-//             console.log(error)
-//         }
-//     }
-
-//     gotoprevpage = async () =>{
-//         try{
-//             let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=ce925a4554cb4328805b38890d26b6df&page=${this.state.page - 1}`);
-//             let parsed_data = await data.json();
-//             this.setState({
-//                 articles: parsed_data.articles,
-//                 page:this.state.page - 1
-//             });
-//         }
-//         catch (error){
-//             console.log(error)
-//         }
-//     }
-
-//     async componentDidMount() {
-//         try {
-//             let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=ce925a4554cb4328805b38890d26b6df&page=1`);
-//             let parsed_data = await data.json();
-//             this.setState({
-//                 articles: parsed_data.articles,
-//             });
-//         } catch (error) {
-//             console.error("Error fetching data:", error);
-//         }
-//     }
-//     render() {
-//         const { articles } = this.state;
-//         return (
-//             <div className='container my-5'>
-//                     <button  type="button" className="btn btn-primary custom-previous">Previous</button>
-//                     <button onClick={this.gotonextpage} type="button" className="btn btn-primary custom-next">Next</button>
-//                 <div className="row">
-//                     {articles.length > 0 ? (
-//                         articles.map((element) => (
-//                             <div className="col-md-4" key={element.url}>
-//                                 <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} url={element.url} />
-//                             </div>
-//                         ))
-//                     ) : (
-//                         <p>Loading...</p>
-//                     )}
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
+export default App;
